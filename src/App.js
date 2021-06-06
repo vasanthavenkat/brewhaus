293efaa742a220ade971fa1f7c42d7c4e14dbb79
vvasa1 <vvasa1@monsanto.com>
2021-06-06 18:03:10 -0500

    Add redirect fallback url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import BeerList from './components/BeerList';
 import BeerDetails from "./components/BeerDetails"
 import Navbar from "./components/Navbar"
+import Redirect from "react-router-dom/es/Redirect"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route path="/brewhaus" exact component={BeerList}/>
           <Route path="/brewhaus/beer-details/:beerId" exact component={BeerDetails}/>
+          <Redirect to='/brewhaus'/>
         </Switch>
       </div>
     </Router>
